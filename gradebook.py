ad1d93df4fd93f95a8998a595c9beb05f8e1c869
Velda Kiara <veldakarimi@gmail.com>
2020-05-12 05:54:07 +0300

    adding zip and gradebook

--- gradebook.py
+++ gradebook.py
@@ -1,3 +1,4 @@
 last_semester_gradebook = [("politics", 80), ("latin", 96), ("dance", 97), ("architecture", 65)]
 subjects = ["physics", "calculus", "poetry","history"]
 grades = [98, 97, 85 ,88]
+gradebook =list(zip(subjects, grades))
